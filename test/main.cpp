6b007be8e8fece4c2fffc3cd3816c3d675397d31
Darshanrgowda54 <darshan.ramesh565@gmail.com>
2025-05-22 14:32:54 +0530

    Darshantest

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+    int a = 10;
+    int b = 20;
+
+    int res = a+b;
+    std::cout<<res<<std::endl;
+
     cout << "Hello World!" << endl;
     return 0;
 }
