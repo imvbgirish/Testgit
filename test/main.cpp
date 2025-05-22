9da3baa0ee659f8c0f03cc745305c5c1e4b6147d
Darshanrgowda54 <darshan.ramesh565@gmail.com>
2025-05-22 14:58:55 +0530

    Darshan-

--- test/main.cpp
+++ test/main.cpp
@@ -8,7 +8,9 @@ int main()
     int b = 20;
 
     int res = a+b;
+	int resu1 = a-b;
     std::cout<<res<<std::endl;
+    std::cout<<res1<<std::endl;
 
     cout << "Hello World!" << endl;
     return 0;
